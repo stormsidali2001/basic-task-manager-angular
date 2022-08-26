8ab3a6d1941bc9cc3bf01c5cb1f5f37419048282
stormsidali2001 <assoulsidali@gmail.com>
2022-08-26 09:20:23 -0700

    importing the http module &using the http client in our task service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,15 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { TASKS } from '../components/mock-tasks';
 import { Task } from '../Task';
+import {HttpClient,HttpHeaders} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:8080/tasks';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
   getTasks():Observable<Task[]>{
-    const tasks = of(TASKS);
-    return tasks;
+   return this.http.get<Task[]>(this.apiUrl);
   }
 }
